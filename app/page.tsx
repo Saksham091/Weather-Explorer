24ddc588589e07bfd2ab37c0a053d8e647589c05
Saksham091 <saksham.agarwal@kalvium.community>
2024-09-25 23:07:18 +0530

    fiveday forecast and mapbox component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import AirPollution from "./Components/AirPollution/AirPollution";
 import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import FeelsLike from "./Components/FeelsLike/FeelsLike";
 import Humidity from "./Components/Humidity/Humidity";
+import Mapbox from "./Components/Mapbox/Mapbox";
 import Navbar from "./Components/Navbar";
 import Population from "./Components/Population/Population";
 import Pressure from "./Components/Pressure/Pressure";
@@ -10,6 +12,8 @@ import Temperature from "./Components/Temperature/Temperature";
 import UvIndex from "./Components/UvIndex/UvIndex";
 import Visibility from "./Components/Visibility/Visibility";
 import Wind from "./Components/Wind/Wind";
+import defaultStates from "./utils/defaultStates";
+import FiveDayForecast from "./Components/FiveDayForecast/FiveDayForecast";
 
 export default function Home() {
   return <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
@@ -17,6 +21,7 @@ export default function Home() {
     <div className="pb-4 flex flex-col gap-4 md:flex-row">
       <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
         <Temperature/>
+        <FiveDayForecast />
       </div>
       <div className="flex flex-col w-full">
         <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -31,7 +36,35 @@ export default function Home() {
           <Visibility />
           <Pressure />
         </div>
+        <div className="mapbox-con mt-4 flex gap-4">
+          <Mapbox />
+          <div className="states flex flex-col gap-3 flex-1 ">
+            <h2 className="flex items-center gap-2 font-medium">
+              Top Large Cities
+            </h2>
+            <div className="flex flex-col gap-4">
+              {
+                defaultStates.map((state, index) => {
+                  return <div key={index} className="border rounded-lg cursor-pointer 
+                  dark:bg-dark-grey shadow-sm dark:shadow-none">
+                    <p className="px-6 py-4">{state.name}</p>
+                  </div>
+                })
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+    <footer className="py-4 flex justify-center pb-8">
+      <p className="footer-text text-sm flex items-center gap-1">
+        Made By
+        <Image src={"/logo-white.svg"}  alt="logo" width={20} height={20} />
+        <a href="https://github.com/Saksham091" target="_blank" className="text-green-300 font-bold">
+        Saksham Agarwal
+        </a>
+      </p>
+    </footer>
   </main>;
 }
